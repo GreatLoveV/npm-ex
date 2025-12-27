7e6ae984cfce693209bcd42a6872aed980873405
ink <92540444+GreatLoveV@users.noreply.github.com>
2025-12-27 17:39:26 +0100

    add test functionality using supertest to test the api with a fake test database

---
+++ controllers/notes.js
@@ -0,0 +1,51 @@
+const notesRouter = require('express').Router()
+const Note = require('../models/note')
+
+notesRouter.get('/', async (req, res) => {
+  const notes = await Note.find({})
+  res.json(notes)
+
+})
+
+notesRouter.get('/:id', async (req, res) => {
+  const note = await Note.findById(req.params.id)
+  if (note){
+    res.json(note)
+  }else{
+    res.status(404).end()
+  }
+})
+
+notesRouter.put('/:id', async (req, res) => {
+  const { content, important } = req.body
+
+  const note = await Note.findById(req.params.id)
+  if (!note) {
+    return res.status(404).end()
+  }
+
+  note.content = content
+  note.important = important
+
+  const updatedNote = await note.save()
+  res.json(updatedNote)
+
+})
+
+notesRouter.post('/', async (req, res) => {
+  const body = req.body
+  const note = new Note({
+    content: body.content,
+    important: body.important || false,
+  })
+  const savedNote = await note.save()
+  res.status(201).json(savedNote)
+
+})
+
+notesRouter.delete('/:id', async (req, res) => {
+  await Note.findByIdAndDelete(req.params.id)
+  res.status(204).end()
+})
+
+module.exports = notesRouter
--- mongo.js
+++ mongo.js
@@ -1,24 +1,16 @@
 const mongoose = require('mongoose')
 
-if (process.argv.length < 3) {
-  console.log('give password as argument')
-  process.exit(1)
-}
-
-// const password = encodeURIComponent(process.argv[2])
-const password = process.argv[2]
-
-const url = `mongodb+srv://sleuthInk531090:${password}@cluster0.ysghu4p.mongodb.net/noteApp?retryWrites=true&w=majority&appName=Cluster0`
+const url =  'mongodb+srv://sleuthInk531090:milkofmorrow@cluster0.ysghu4p.mongodb.net/testNoteApp?retryWrites=true&w=majority&appName=Cluster0'
 
 mongoose.set('strictQuery', false)
 
 mongoose.connect(url, { family:4 })
 
 const noteSchema = new mongoose.Schema({
-  content: String,
-  date: {
-    type: Date,
-    default: Date.now
+  content: {
+    type: String,
+    minlength: 5,
+    required: true
   },
   important: Boolean
 })
@@ -26,7 +18,7 @@ const noteSchema = new mongoose.Schema({
 const Note= mongoose.model('Note', noteSchema)
 
 const note = new Note({
-  content: 'Date test',
+  content: 'HTML is easy',
   important: true,
 })
 
@@ -34,5 +26,3 @@ note.save().then(() => {
   console.log('note saved!')
   mongoose.connection.close()
 })
-
-
